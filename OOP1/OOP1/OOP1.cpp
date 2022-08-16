07f9fd5946134b42185dad87d2e0a351a4b97669
deLong34 <delong@bk.ru>
2022-08-17 00:01:06 +0300

    1 задание

--- OOP1/OOP1/OOP1.cpp
+++ OOP1/OOP1/OOP1.cpp
@@ -1,10 +1,65 @@
 ﻿// 
 
 #include <iostream>
+#include <cstdint>
+#include <string>
 
-class 
+using namespace std;
 
-int main()
+///////////////////==1==////////////////////
+
+class Power
 {
-    std::cout << "Hello World!\n";
-}
+private:
+    float a, b;
+
+public:
+    Power()
+    {
+        a = 2.15;
+        b = 3;
+    }
+    void set(float _a, float _b)
+    {
+        a = _a;
+        b = _b;
+    }
+
+    void calculate()
+    {
+       // float c;
+       // c = pow(a, b);
+       // cout << "Результат: " << c << endl;
+       //можно одной строкой:
+        cout << "Результат: " << pow(a, b) << endl;
+    }
+};
+
+//////////////////==2==//////////////////////
+
+class RGBA
+{
+public:
+    RGBA()
+    {
+        m_red = 0;
+        m_green = 0;
+        m_blue = 0;
+        m_alpha = 255;
+    };
+    ~RGBA();
+
+private:
+    uint8_t m_red, m_green, m_blue, m_alpha;
+
+
+};
+
+    int main()
+    {
+        setlocale(LC_ALL, "Rus");
+        Power P;
+        //P.set(2.3, 3);
+        P.calculate();
+
+    }
